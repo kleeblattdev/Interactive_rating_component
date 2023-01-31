b25bdf118c133cf84480a2fb49b273d10fca4626
karinlee <karinlee92@gmail.com>
2023-01-31 16:47:43 +0100

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+// Input
+//Cards
+const ratingCard = document.querySelector('.ratingCard');
+const thankYouCard = document.querySelector('.thankYouCard');
+//Rating
+const ratingOne = document.querySelector("#ratingOne");
+const ratingTwo = document.querySelector("#ratingTwo");
+const ratingThree = document.querySelector("#ratingThree");
+const ratingFour = document.querySelector("#ratingFour");
+const ratingFive = document.querySelector("#ratingFive");
+
+
+const btnSubmit = document.querySelector("#submit");
+
+//Output
+
+ratingOne.addEventListener("click", (event) =>{
+    event.preventDefault();
+    ratingOne.classList.toggle("selectedRating");
+})
+
+ratingTwo.addEventListener("click", (event) =>{
+    event.preventDefault();
+    ratingTwo.classList.toggle("selectedRating");
+})
+
+ratingThree.addEventListener("click", (event) =>{
+    event.preventDefault();
+    ratingThree.classList.toggle("selectedRating");
+})
+
+ratingFour.addEventListener("click", (event) =>{
+    event.preventDefault();
+    ratingFour.classList.toggle("selectedRating");
+})
+
+ratingFive.addEventListener("click", (event) =>{
+    event.preventDefault();
+    ratingFive.classList.toggle("selectedRating");
+})
+
+btnSubmit.addEventListener("click", () => {
+    ratingCard.style.display = "none";
+    thankYouCard.style.display = "block";
+})
