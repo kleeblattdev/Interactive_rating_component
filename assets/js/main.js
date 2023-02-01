6b7aaa5562c962510dd7ca4136906404b673b4f6
karinlee <karinlee92@gmail.com>
2023-02-01 14:37:58 +0100

    changed the input button to buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,7 @@
 const ratingCard = document.querySelector('.ratingCard');
 const thankYouCard = document.querySelector('.thankYouCard');
 //Rating
-const ratingOne = document.querySelector("#ratingOne");
-const ratingTwo = document.querySelector("#ratingTwo");
-const ratingThree = document.querySelector("#ratingThree");
-const ratingFour = document.querySelector("#ratingFour");
-const ratingFive = document.querySelector("#ratingFive");
+const ratingSelected = document.querySelector(".rating");
 
 
 const btnSubmit = document.querySelector("#submit");
@@ -16,34 +12,14 @@ const btnSubmit = document.querySelector("#submit");
 const output = document.querySelector("#selected");
 //
 
-ratingOne.addEventListener("click", (event) =>{
+ratingSelected.addEventListener("click", (event) =>{
     event.preventDefault();
-    ratingOne.classList.toggle("selectedRating");
-})
-
-ratingTwo.addEventListener("click", (event) =>{
-    event.preventDefault();
-    ratingTwo.classList.toggle("selectedRating");
-})
-
-ratingThree.addEventListener("click", (event) =>{
-    event.preventDefault();
-    ratingThree.classList.toggle("selectedRating");
-})
-
-ratingFour.addEventListener("click", (event) =>{
-    event.preventDefault();
-    ratingFour.classList.toggle("selectedRating");
-})
-
-ratingFive.addEventListener("click", (event) =>{
-    event.preventDefault();
-    ratingFive.classList.toggle("selectedRating");
+    ratingSelected.classList.toggle("selectedRating");
 })
 
 btnSubmit.addEventListener("click", () => {
     ratingCard.style.display = "none";
     //Value was ausgewählt wurde fehlt 
-    output.innerHTML = "You selected" + + "out of 5";
+    output.innerHTML = "You selected " + ratingSelected.value + " out of 5";
     thankYouCard.style.display = "block";
 })
